23b36c7e06850107cb9def0f4bcb48bd59d0af35
Yehonathan Sharvit <viebel@gmail.com>
2019-12-15 10:04:30 +0200

    retry request only if the error status is 429

--- src/google_api_clj/net_utils.clj
+++ src/google_api_clj/net_utils.clj
@@ -1,20 +1,21 @@
 (ns google-api-clj.net-utils
   (:require
+   [google-api-clj.google-utils :refer [error-code]]
    [taoensso.timbre :as timbre]
    [clojure.pprint :as pp]
    [clojure.java.io :as io]))
 
-(def max-attempts 4) ;; Overall the request should take less than 30 seconds
+(def max-attempts 5) ;; Overall the request should take less than 30 seconds
 (def grace-period 1000)
 
 (defn execute [request]
   ;; exponential backoff according to https://developers.google.com/drive/api/v3/handle-errors#exponential-backoff
-  (loop [attempt 0]
+  (loop [attempt 1]
     (let [[status result] (try
-                            (timbre/info (str "Google API - attempt " (inc attempt) "/" max-attempts ". request: ") request (with-out-str (pp/pprint request)))
+                            (timbre/info (str "Google API - attempt " attempt "/" max-attempts ". request: ") request (with-out-str (pp/pprint request)))
                             [:ok (.execute request)]
                             (catch Throwable ex
-                              (if (< attempt max-attempts)
+                              (if (and (= 429 (error-code ex)) (< attempt max-attempts))
                                 [:retry ex]
                                 [:error ex])))]
       (condp = status
