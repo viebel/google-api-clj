938e6e67580dc879380134a4d276d88273c161f6
Yehonathan Sharvit <viebel@gmail.com>
2021-12-16 14:22:01 +0200

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.taoensso/timbre "4.10.0"]
-                 [com.google.api-client/google-api-client           "1.28.0"]
+                 [com.google.api-client/google-api-client           "1.32.1"]
+                 [com.google.http-client/google-http-client-jackson2  "1.39.2"]
                  [com.google.oauth-client/google-oauth-client-jetty "1.28.0"]
                  [com.google.apis/google-api-services-sheets        "v4-rev20190109-1.28.0"]
                  [com.google.apis/google-api-services-drive         "v3-rev20181213-1.28.0"]])
